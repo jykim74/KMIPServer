d41e28f7ecdbc088c74ef2649f8d79ffc5c85b71
jykim <jykim74@gmail.com>
2019-12-30 18:03:49 +0900

    Base work

--- kms_srv/kms_proc.c
+++ kms_srv/kms_proc.c
@@ -4,19 +4,118 @@
 
 #include "kmip.h"
 #include "js_bin.h"
+#include "js_pkcs11.h"
+
+extern JSP11_CTX    *g_pP11CTX;
+extern CK_SESSION_HANDLE    g_hSession;
 
 int runGet( const RequestBatchItem *pReqItem, ResponseBatchItem *pRspItem )
 {
+    GetRequestPayload *grp = (GetRequestPayload *)pReqItem->request_payload;
+
+
+
     return 0;
 }
 
 int runCreate(const RequestBatchItem *pReqItem, ResponseBatchItem *pRspItem)
 {
+    int ret = 0;
+    CreateRequestPayload *crp = (CreateRequestPayload *)pReqItem->request_payload;
+
+    CK_ATTRIBUTE        sTemplate[20];
+    long                uCount = 0;
+    CK_OBJECT_HANDLE hObject = 0;
+    CK_MECHANISM    sMech;
+
+    int             *pnAlg = NULL;
+    int             *pnLen = NULL;
+    int             *pnMask = NULL;
+
+    memset( &sMech, 0x00, sizeof(sMech));
+
+    if( crp->object_type == KMIP_OBJTYPE_SYMMETRIC_KEY )
+    {
+        CK_OBJECT_CLASS keyClass = CKO_SECRET_KEY;
+        sTemplate[uCount].type = CKA_CLASS;
+        sTemplate[uCount].pValue = &keyClass;
+        sTemplate[uCount].ulValueLen = sizeof(keyClass);
+        uCount++;
+
+        TemplateAttribute *ta = crp->template_attribute;
+        for( int i = 0; i < ta->attribute_count; i++ )
+        {
+            if( ta->attributes[i].type == KMIP_ATTR_CRYPTOGRAPHIC_ALGORITHM )
+            {
+                pnAlg = ta->attributes[i].value;
+
+                if( *pnAlg == KMIP_CRYPTOALG_AES )
+                {
+                    sMech.mechanism = JS_PKCS11_GetCKMType( "CKM_AES_KEY_GEN" );
+                }
+            }
+            else if( ta->attributes[i].type == KMIP_ATTR_CRYPTOGRAPHIC_LENGTH )
+            {
+                pnLen = ta->attributes[i].value;
+
+                sTemplate[uCount].type = CKA_VALUE_LEN;
+                sTemplate[uCount].pValue = pnLen;
+                sTemplate[uCount].ulValueLen = sizeof(*pnLen);
+                uCount++;
+            }
+            else if( ta->attributes[i].type == KMIP_ATTR_CRYPTOGRAPHIC_USAGE_MASK )
+            {
+                pnMask = ta->attributes[i].value;
+
+                if( *pnMask | KMIP_CRYPTOMASK_ENCRYPT )
+                {
+                    sTemplate[uCount].type = CKA_ENCRYPT;
+                    sTemplate[uCount].pValue = CK_TRUE;
+                    sTemplate[uCount].ulValueLen = sizeof(CK_TRUE);
+                    uCount++;
+                }
+                else if( *pnMask | KMIP_CRYPTOMASK_DECRYPT )
+                {
+                    sTemplate[uCount].type = CKA_DECRYPT;
+                    sTemplate[uCount].pValue = CK_TRUE;
+                    sTemplate[uCount].ulValueLen = sizeof(CK_TRUE);
+                    uCount++;
+                }
+            }
+        }
+
+        ret = JS_PKCS11_GenerateKey( g_pP11CTX, g_hSession, &sMech, sTemplate, uCount, &hObject );
+        if( ret != CKR_OK )
+        {
+            fprintf( stderr, "fail to run generate key(%d)\n", ret );
+        }
+
+
+    }
+
+    CreateResponsePayload   *pld = (CreateRequestPayload *)JS_calloc( 1, sizeof(CreateResponsePayload ) );
+
+    pRspItem->operation = pReqItem->operation;
+
+    if( ret == 0 )
+    {
+        pRspItem->result_status = KMIP_STATUS_SUCCESS;
+        pld->unique_identifier = strdup( "uuid" );
+        pRspItem->response_payload = pld;
+    }
+    else
+    {
+        pRspItem->result_status = KMIP_STATUS_OPERATION_FAILED;
+    }
+
     return 0;
 }
 
 int runDestroy(const RequestBatchItem *pReqItem, ResponseBatchItem *pRspItem)
 {
+    DestroyRequestPayload *drp = (DestroyRequestPayload *)pReqItem->request_payload;
+
+
     return 0;
 }
 
@@ -56,12 +155,18 @@ int procBatchItem( const RequestBatchItem *pReqItem, ResponseBatchItem *pRspItem
     {
 
     }
+    else
+    {
+        fprintf( stderr, "not support operation(%d)\n", pReqItem->operation );
+        return -1;
+    }
 
     return 0;
 }
 
 int procKMS( const BIN *pReq, BIN *pRsp )
 {
+    int ret = 0;
     KMIP ctx = {0};
     RequestMessage  reqm = {0};
     ResponseMessage rspm = {0};
@@ -76,10 +181,13 @@ int procKMS( const BIN *pReq, BIN *pRsp )
 
     for( int i = 0; i < reqm.batch_count; i++ )
     {
+        ResponseBatchItem rspBatch = {0};
 
+        ret = procBatchItem( reqm.batch_items, &rspBatch );
     }
 
     kmip_set_buffer( &ctx, NULL, 0 );
+
     kmip_print_response_message( &rspm );
     kmip_encode_response_message( &ctx, &reqm );
 
--- kms_srv/kms_srv.c
+++ kms_srv/kms_srv.c
@@ -10,12 +10,14 @@
 #include "kms_srv.h"
 #include "js_ssl.h"
 #include "js_kmip.h"
+#include "js_pkcs11.h"
 
 SSL_CTX     *g_pSSLCTX = NULL;
 BIN         g_binPri = {0,0};
 BIN         g_binCert = {0,0};
 BIN         g_binCACert = {0,0};
-
+JSP11_CTX   *g_pP11CTX = NULL;
+CK_SESSION_HANDLE   g_hSession = -1;
 
 int KMS_Service( JThreadInfo *pThInfo )
 {
@@ -79,9 +81,11 @@ end:
 
 int Init()
 {
+    int ret = 0;
     const char *pCACertPath = "/Users/jykim/work/certs/root_cert.der";
     const char *pCertPath = "/Users/jykim/work/certs/server_cert.der";
     const char *pPriPath = "/Users/jykim/work/certs/server_prikey.der";
+    const char *pP11Path = "/usr/local/lib/softhsm/libsofthsm2.so";
 
     JS_BIN_fileRead( pCACertPath, &g_binCACert );
     JS_BIN_fileRead( pCertPath, &g_binCert );
@@ -90,6 +94,67 @@ int Init()
     JS_SSL_initServer( &g_pSSLCTX );
     JS_SSL_setCertAndPriKey( g_pSSLCTX, &g_binPri, &g_binCert );
     JS_SSL_setClientCACert( g_pSSLCTX, &g_binCACert );
+
+    JS_PKCS11_LoadLibrary( &g_pP11CTX, pP11Path );
+
+    ret = loginHSM();
+
+    return ret;
+}
+
+int loginHSM()
+{
+    int ret = 0;
+    int nFlags = 0;
+
+    CK_SESSION_HANDLE hSession = NULL;
+    CK_ULONG uSlotCnt = 0;
+    CK_SLOT_ID  sSlotList[10];
+
+    char *pPin = "1234";
+    long uPinLen = 4;
+    int nUserType = 0;
+
+    nFlags |= CKF_RW_SESSION;
+    nFlags |= CKF_SERIAL_SESSION;
+    nUserType = CKU_USER;
+
+    ret = JS_PKCS11_Initialize( g_pP11CTX );
+    if( ret != CKR_OK )
+    {
+        fprintf( stderr, "fail to run initialize(%d)\n", ret );
+        return -1;
+    }
+
+    ret = JS_PKCS11_GetSlotList2( g_pP11CTX, CK_TRUE, sSlotList, &uSlotCnt );
+    if( ret != CKR_OK )
+    {
+        fprintf( stderr, "fail to run getSlotList fail(%d)\n", ret );
+        return -1;
+    }
+
+    if( uSlotCnt < 1 )
+    {
+        fprintf( stderr, "there is no slot(%d)\n", uSlotCnt );
+        return -1;
+    }
+
+    ret = JS_PKCS11_OpenSession( g_pP11CTX, sSlotList[0], nFlags, &hSession );
+    if( ret != CKR_OK )
+    {
+        fprintf( stderr, "fail to run opensession(%s:%x)\n", JS_PKCS11_GetErrorMsg(ret), ret );
+        return -1;
+    }
+
+    ret = JS_PKCS11_Login( g_pP11CTX, hSession, nUserType, pPin, uPinLen );
+    if( ret != 0 )
+    {
+        fprintf( stderr, "fail to run login hsm(%d)\n", ret );
+        return -1;
+    }
+
+    g_hSession = hSession;
+    printf( "HSM login ok\n" );
 
     return 0;
 }
